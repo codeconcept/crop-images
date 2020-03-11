ca795e3ee4cb376337e55c73e9663b7138e2b241
codeconcept <contact@codeconcept.fr>
2020-03-11 17:33:58 +0100

    Cropping

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const uploadedImageDiv = document.getElementById("uploadedImage");
 const fileUpload = document.getElementById("fileUpload");
 fileUpload.addEventListener("change", getImage, false);
+let cropper = null;
+const cropButton = document.getElementById("cropButton");
+cropButton.addEventListener("click", cropImage);
 
 function getImage() {
   console.log("images", this.files[0]);
@@ -12,12 +15,30 @@ function getImage() {
   let newImg = new Image(imageToProcess.width, imageToProcess.height);
   newImg.src = imageToProcess;
   newImg.src = URL.createObjectURL(imageToProcess);
+  newImg.id = "myGreatImage";
   uploadedImageDiv.style.border = "4px solid #FCB514";
   uploadedImageDiv.appendChild(newImg);
 
-  processImage(imageToProcess);
+  processImage();
 }
 
-function processImage(image) {
-  // TODO
+function processImage() {
+  const myGreatImage = document.getElementById("myGreatImage");
+  const croppedImage = document.getElementById("croppedImage");
+
+  cropper = new Cropper(myGreatImage, {
+    aspectRatio: 16 / 9,
+    crop(event) {
+      console.log(event);
+      const canvas = this.cropper.getCroppedCanvas();
+      croppedImage.src = canvas.toDataURL("image/png");
+    }
+  });
+}
+
+function cropImage() {
+  const imgurl = cropper.getCroppedCanvas().toDataURL();
+  const img = document.createElement("img");
+  img.src = imgurl;
+  document.getElementById("cropResult").appendChild(img);
 }
